4498d47cf3f532d72ca912628474eec06e2cbdbc
Alexander Rose <acrosemail@gmail.com>
2020-10-14 14:27:02 -0500

    Prototype result page, changed the frontpage, and changed the hamburger bar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var form = document.getElementById("f1");
 var input = ""
-form.onsubmit = function(event){
+form.onsubmit = function (event) {
     event.preventDefault();
     input = form.t1.value;
     console.log("Restaurant: " + input);
@@ -9,8 +9,11 @@ form.onsubmit = function(event){
       // filterByRisk(form.t1.value);
       // filterByAddress(input)
      */
-      filterByZip(input);
+    // filterByZip(input);
+    filterByName(form.t1.value);
+
 }
+
 function loadJson() {
     $.ajax({
         url: "https://data.cityofchicago.org/resource/4ijn-s7e5.json",
@@ -37,12 +40,20 @@ function loadJson() {
         }
     });
 }
-function filterByName(nameInputted) {
 
+function filterByName(nameInputted) {
+    let name;
+    /* Converts the correct input into the string, if we dont click submit */
+    if (typeof nameInputted != "string") {
+        name = form.t1.value;
+        name = name.toUpperCase();
+        name = name.trimEnd();
+    } else {
+        name = nameInputted.toUpperCase()
+    }
     /* All you need to do for a user inputted name is to convert the restaurant name into uppercase /*
 
      */
-    const name = nameInputted.toUpperCase()
     $.ajax({
         url: "https://data.cityofchicago.org/resource/4ijn-s7e5.json",
         type: "GET",
@@ -69,26 +80,33 @@ function filterByName(nameInputted) {
         }
     });
 }
+
 function filterByRisk(riskInputted) {
-  /* A risk can consist of three different values
-    1. risk: "Risk 1 (High)"
-    2. risk: "Risk 2 (Medium)"
-    3. risk: "Risk 3 (Low)"
-    Thus, we need to convert whatever the user inputs into one of these values.
- */
+    let risk;
+    /* Converts the correct input into the string, if we dont click submit */
+    if (typeof riskInputted != "string") {
+        risk = form.t1.value;
+        risk = risk.toUpperCase();
+    } else {
+        risk = riskInputted.toUpperCase();
+    }
+
+    /* A risk can consist of three different values
+      1. risk: "Risk 1 (High)"
+      2. risk: "Risk 2 (Medium)"
+      3. risk: "Risk 3 (Low)"
+      Thus, we need to convert whatever the user inputs into one of these values.
+   */
     const highRisk = "1 (HIGH)"
     const mediumRisk = "2 (MEDIUM)"
     const lowRisk = "3 (LOW)"
-    let risk = riskInputted.toUpperCase();
 
     if (highRisk.includes(risk)) {
-      risk = "Risk 1 (High)"
-    }
-    else if (mediumRisk.includes(risk)) {
-      risk = "Risk 2 (Medium)"
-    }
-    else if (lowRisk.includes(risk)) {
-      risk = "Risk 3 (Low)"
+        risk = "Risk 1 (High)"
+    } else if (mediumRisk.includes(risk)) {
+        risk = "Risk 2 (Medium)"
+    } else if (lowRisk.includes(risk)) {
+        risk = "Risk 3 (Low)"
     }
     // const risk = nameInputted.toUpperCase()
     $.ajax({
@@ -118,13 +136,22 @@ function filterByRisk(riskInputted) {
         }
     });
 }
+
 function filterByZip(zipInputted) {
-  /* All you need to do for this is convert the address to upper case,
-    // test int = 60655
- */
-  let zip;
-  zip = zipInputted.trimEnd(); // For user error
-  console.log(zip);
+    /* All you need to do for this is convert the address to upper case,
+      // test int = 60655
+   */
+    let zip;
+    /* Converts the correct input into the string, if we dont click submit */
+    if (typeof zipInputted != "string") {
+        zip = form.t1.value;
+        zip = zip.toUpperCase();
+    } else {
+        zip = zipInputted.toUpperCase();
+    }
+
+    zip = zip.trimEnd(); // For user error
+    console.log(zip);
     $.ajax({
         url: "https://data.cityofchicago.org/resource/4ijn-s7e5.json",
         type: "GET",
@@ -152,11 +179,25 @@ function filterByZip(zipInputted) {
         }
     });
 }
+
 function filterByAddress(addressInputted) {
     /* All you need to do for this is convert the address to upper case and a space to the end, for socratas weird crap
         test string = "4635 W 63RD ST"
      */
-    let address = addressInputted.trimEnd(); // For user error
+
+    let address;
+    /* Converts the correct input into the string, if we dont click submit */
+    if (typeof addressInputted != "string") {
+        address = form.t1.value;
+        address = address.toUpperCase();
+        address = address.trimEnd();
+        address = address + ' ';
+    } else {
+        address = addressInputted.toUpperCase();
+    }
+
+
+    address = address.trimEnd(); // For user error
     address = address.toUpperCase() + ' ';
     console.log(address);
     $.ajax({
@@ -185,4 +226,13 @@ function filterByAddress(addressInputted) {
             // console.log(data[i]["results"]);
         }
     });
+}
+
+function menuFunction() {
+    var menu = document.getElementById("hamMenu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
